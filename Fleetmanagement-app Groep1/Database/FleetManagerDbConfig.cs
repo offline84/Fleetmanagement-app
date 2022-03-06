168a5a305c411855e22d98b8ff13e3a29eb3fec1
offline84 <ken_decrop@hotmail.com>
2022-03-06 20:04:55 +0100

    Added Database configuration and started with defining the database context (unfinished)

---
+++ Fleetmanagement-app Groep1/Database/FleetManagerDbConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.SqlServer;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fleetmanagement_app_Groep1.Database
+{
+    public class FleetManagerDbConfig: DbConfiguration
+    {
+        public FleetManagerDbConfig()
+        {
+            SetProviderServices(SqlProviderServices.ProviderInvariantName, SqlProviderServices.Instance);
+        }
+    }
+}
---
+++ Fleetmanagement-app Groep1/Database/FleetmanagerContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Data.Entity;
+
+namespace Fleetmanagement_app_Groep1.Database
+{
+    [DbConfigurationType(typeof(FleetManagerDbConfig))]
+    public class FleetmanagerContext : DbContext
+    {
+        private string _connectionstring;
+
+        public string Connectionstring
+        {
+            get => _connectionstring;
+            set
+            {
+                var config = new ConfigurationBuilder()
+                   .AddJsonFile("dbsettings.json", optional: true)
+                   .AddEnvironmentVariables()
+                   .Build();
+
+                string datasource = config.GetValue<string>("DataSource");
+                string catalog = config.GetValue<string>("Catalog");
+
+                _connectionstring = $"@Data Source={datasource}; Initial Catalog={catalog};Integrated Security=true";
+            }
+        }
+
+        public FleetmanagerContext(string connection = Connectionstring): base(connection);
+        {
+
+        }
+    }
+}
