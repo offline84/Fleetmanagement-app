40d7ddd5221b6f8f6c0e67942912218fcac4c909
offline84 <ken_decrop@hotmail.com>
2022-03-08 00:58:24 +0100

    database has been implemented

--- Fleetmanagement-app Groep1/Database/FleetmanagerContext.cs
+++ Fleetmanagement-app Groep1/Database/FleetmanagerContext.cs
@@ -1,33 +1,20 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Diagnostics;
 using System.Data.Entity;
+using Microsoft.EntityFrameworkCore;
+using DbContext = Microsoft.EntityFrameworkCore.DbContext;
 
 namespace Fleetmanagement_app_Groep1.Database
 {
     [DbConfigurationType(typeof(FleetManagerDbConfig))]
     public class FleetmanagerContext : DbContext
     {
-        private string _connectionstring;
-
-        public string Connectionstring
+        private const string ConnectionString =
+            @"Data Source=(localdb)\\mssqllocaldb;Initial Catalog=FleetManagerdb;Integrated Security=true;";
+                
+                
+        public FleetmanagerContext(DbContextOptions<FleetmanagerContext> options ) : base(options)
         {
-            get => _connectionstring;
-            set
-            {
-                var config = new ConfigurationBuilder()
-                   .AddJsonFile("dbsettings.json", optional: true)
-                   .AddEnvironmentVariables()
-                   .Build();
-
-                string datasource = config.GetValue<string>("DataSource");
-                string catalog = config.GetValue<string>("Catalog");
-
-                _connectionstring = $"@Data Source={datasource}; Initial Catalog={catalog};Integrated Security=true";
-            }
-        }
-
-        public FleetmanagerContext(string connection = Connectionstring): base(connection);
-        {
-
         }
     }
 }
--- Fleetmanagement-app Groep1/Program.cs
+++ Fleetmanagement-app Groep1/Program.cs
@@ -1,5 +1,8 @@
+using Fleetmanagement_app_Groep1.Database;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -13,7 +16,26 @@ namespace Fleetmanagement_app_Groep1
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            //migratie en aanmaken van de database indien deze nog niet bestaat
+            using (var scope = host.Services.CreateScope())
+            {
+                try
+                {
+                    var context = scope.ServiceProvider.GetService<FleetmanagerContext>();
+                    //check of de connectie kan gemaakt worden met de databank
+                    if(!context.Database.CanConnect())
+                        //Maak de databank
+                        context.Database.Migrate();
+                }
+                catch (Exception e)
+                {
+                    var logbook = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
