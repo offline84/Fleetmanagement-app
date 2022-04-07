b38e8f8fec10f243899761865abd68d3832c2fb7
Cédric van Aubel <cedric.vanaubel@gmail.com>
2022-04-07 14:31:03 +0200

    Created Tankkaart Test Class and started implementing Tankkaart Repo

---
+++ Fleetmanagement Unit Tests/TankkaartRepositoryTests.cs
@@ -0,0 +1,21 @@
+﻿using Fleetmanagement_app_Groep1.Database;
+using Fleetmanagement_app_Groep1.Helpers;
+using Fleetmanagement_app_BLL.Repository;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Fleetmanagement_Unit_Tests
+{
+    public class TankkaartRepositoryTests
+    {
+        private static FleetmanagerContext _context = new FleetmanagerContext(DbContextHelper.GetDbContextOptions());
+        private static readonly ILogger _logger;
+        private TankkaartRepository _TankkaartRepository = new TankkaartRepository(_context, _logger);
+
+
+
+    }
+}
--- Fleetmanagement-app-BLL/Repository/TankkaartRepository.cs
+++ Fleetmanagement-app-BLL/Repository/TankkaartRepository.cs
@@ -1,7 +1,13 @@
 ﻿using Fleetmanagement_app_Groep1.Database;
 using Fleetmanagement_app_Groep1.Entities;
 using Fleetmanagement_app_BLL.GenericRepository;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
 
 namespace Fleetmanagement_app_BLL.Repository
 {
@@ -9,7 +15,50 @@ namespace Fleetmanagement_app_BLL.Repository
     {
         public TankkaartRepository(FleetmanagerContext context, ILogger logger) : base(context, logger)
         {
-
+            _context = context;
         }
+
+        public override async Task<bool> Add(Tankkaart tankkaart)
+        {
+            List<ToewijzingBrandstofTankkaart> toewijzingbrandstof = new List<ToewijzingBrandstofTankkaart>();
+            tankkaart.Kaartnummer = tankkaart.Kaartnummer.Trim();
+
+            if (tankkaart.GeldigheidsDatum == null | tankkaart.Kaartnummer == "")
+            {
+                return false;
+            }
+
+            await _context.ToewijzingBrandstofTankkaarten.AddRangeAsync(toewijzingbrandstof);
+            await _dbSet.AddAsync(tankkaart);
+
+            return true;
+        }
+
+        public override async Task<bool> Delete(string id)
+        {
+            throw new NotImplementedException();      
+        }
+
+        public override async Task<IEnumerable<Tankkaart>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override async Task<Tankkaart> GetById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<bool> Update(Tankkaart tankkaart)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override async Task<IEnumerable<Tankkaart>> Find(Expression<Func<Tankkaart, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
     }
+
 }
