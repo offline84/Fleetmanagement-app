70e6b5fa674aa71d2a9e0f3045be3b0013be8ccc
Cédric van Aubel <cedric.vanaubel@gmail.com>
2022-04-15 18:03:32 +0200

    Tankkaart Repo and Tests Continued

--- Fleetmanagement Unit Tests/TankkaartRepositoryTests.cs
+++ Fleetmanagement Unit Tests/TankkaartRepositoryTests.cs
@@ -1,13 +1,14 @@
-﻿using Fleetmanagement_app_Groep1.Database;
+﻿using Fleetmanagement_app_DAL.Database;
+using Fleetmanagement_app_DAL.Entities;
 using Fleetmanagement_app_Groep1.Helpers;
 using Fleetmanagement_app_BLL.Repository;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Text;
-using Xunit;
-using Fleetmanagement_app_Groep1.Entities;
 using System.Threading.Tasks;
+using System.Linq;
+using Xunit;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 
 namespace Fleetmanagement_Unit_Tests
 {
@@ -15,13 +16,11 @@ namespace Fleetmanagement_Unit_Tests
     {
         private static FleetmanagerContext _context = new FleetmanagerContext(DbContextHelper.GetDbContextOptions());
         private ILoggerFactory _loggerfactory = new LoggerFactory();
-        //public ILogger _logger;
-        private TankkaartRepository _repo; // = new TankkaartRepository(_context, _logger);
+        private TankkaartRepository _repo;
 
         public TankkaartRepositoryTests()
         {
-            ILoggerFactory loggerfactory = new LoggerFactory();
-            _repo = new TankkaartRepository(_context, _loggerfactory.CreateLogger("Tankkartlogs"));
+            _repo = new TankkaartRepository(_context, _loggerfactory.CreateLogger("TankkaartTestlogs"));
         }
 
         internal Tankkaart TestCreateTankkaart(string kaartnummer, DateTime geldigheidsDatum, int pincode)
@@ -31,18 +30,148 @@ namespace Fleetmanagement_Unit_Tests
             return tankkaart;
         }
 
+        internal Tankkaart GetTankkaart1()
+        {
+            var tankkaart = new Tankkaart("123456789", DateTime.Now);
+            tankkaart.Pincode = 1234;
+            var toewijzingen = new List<ToewijzingBrandstofTankkaart>();
+
+            ToewijzingBrandstofTankkaart toewijzing1 = new ToewijzingBrandstofTankkaart()
+            {
+                Tankkaart = tankkaart,
+                Brandstof = _context.Brandstof.Where(b => b.TypeBrandstof == "euro 95").Single()
+            };
+            ToewijzingBrandstofTankkaart toewijzing2 = new ToewijzingBrandstofTankkaart()
+            {
+                Tankkaart = tankkaart,
+                Brandstof = _context.Brandstof.Where(b => b.TypeBrandstof == "euro 98").Single()
+            };
+            toewijzingen.Add(toewijzing1);
+            toewijzingen.Add(toewijzing2);
+            tankkaart.MogelijkeBrandstoffen = toewijzingen;
+
+            return tankkaart;
+        }
+
+        internal void Cleanup(Tankkaart tankkaart)
+        {
+            var tankkaartToDelete = _context.Tankkaarten.Where(t => t.Kaartnummer == tankkaart.Kaartnummer).AsNoTracking().FirstOrDefault();
+
+            if (tankkaartToDelete != null)
+            {
+                _context.Tankkaarten.Remove(tankkaartToDelete);
+            }
+        }
+
         [Theory]
-        [InlineData("123456789","11/04/2022",12345)]
+        [InlineData("123456789", "11/04/2022", 12345)]
         [InlineData("15645", "21/12/2021", 123456)]
         [InlineData("98jhjg84", "01/01/2020", 954865)]
-        public async Task AddTankkaart_DbNotEmptyAsync(string kaartnummer, string geldigheidsDatumString, int pincode)
+        public async Task AddTankkaart_TankkaartIsCreated_Aangemaakt(string kaartnummer, string geldigheidsDatumString, int pincode)
         {
             DateTime geldigheidsDatum = DateTime.Parse(geldigheidsDatumString);
             var tankkaart = TestCreateTankkaart(kaartnummer, geldigheidsDatum, pincode);
 
-            Assert.NotNull(tankkaart);
-            Assert.True(await _repo.Add(tankkaart));
+            await _repo.Add(tankkaart);
+            var addedTankkaart = await _repo.GetById(tankkaart.Kaartnummer);
+
+            Assert.NotNull(addedTankkaart);
+            Cleanup(addedTankkaart);
         }
+
+        [Theory]
+        [InlineData("123456789", 12345)]
+        [InlineData("15645", 123456)]
+        [InlineData("98jhjg84", 954865)]
+        public async Task AddTankkaart_TankkaartZonderGeldigheidsDatum_NietAangemaakt(string kaartnummer, int pincode)
+        {
+            DateTime geldigheidsDatum = default;
+            var tankkaart = TestCreateTankkaart(kaartnummer, geldigheidsDatum, pincode);
+
+            await _repo.Add(tankkaart);
+            var addedTankkaart = await _repo.GetById(tankkaart.Kaartnummer);
+
+            Assert.Null(addedTankkaart);
+            Cleanup(addedTankkaart);
+        }
+
+        [Theory]
+        [InlineData("11/04/2022", 12345)]
+        [InlineData("21/12/2021", 123456)]
+        [InlineData("01/01/2020", 954865)]
+        public async Task AddTankkaart_TankkaartZonderKaartnummer_NietAangemaakt(string geldigheidsDatumString, int pincode)
+        {
+            DateTime geldigheidsDatum = DateTime.Parse(geldigheidsDatumString);
+            var kaartnummer = "";
+            var tankkaart = TestCreateTankkaart(kaartnummer, geldigheidsDatum, pincode);
+
+            await _repo.Add(tankkaart);
+            var addedTankkaart = await _repo.GetById(tankkaart.Kaartnummer);
+
+            Assert.Null(addedTankkaart);
+            Cleanup(addedTankkaart);
+        }
+
+        [Fact]
+        public async Task AddTankkaart_TankkaartBrandstofToekkeningen_Aangemaakt()
+        {
+            var tankkaart = GetTankkaart1();
+
+            await _repo.Add(tankkaart);
+            var addedTankkaart = await _repo.GetById(tankkaart.Kaartnummer);
+
+            Assert.NotNull(addedTankkaart);
+            Assert.True(addedTankkaart.MogelijkeBrandstoffen.Count() == 2);
+            Cleanup(addedTankkaart);
+        }
+
+
+        [Fact]
+        public async Task DeleteTankkaart_TankkaartWordtGearchiveerd()
+        {
+            var tankkaart = GetTankkaart1();
+            await _repo.Add(tankkaart);
+            var addedTankkaart = await _repo.GetById(tankkaart.Kaartnummer);
+            await _repo.Delete(tankkaart.Kaartnummer);
+            var deletedTankkaart = await _repo.GetById(tankkaart.Kaartnummer);
+
+            Assert.NotNull(addedTankkaart);
+            Assert.NotNull(deletedTankkaart);
+            Assert.True(deletedTankkaart.IsGearchiveerd = true);
+            Cleanup(addedTankkaart);
+        }
+        [Fact]
+        public async Task GetAllActief_NaDeleteEenMinderDanGetAll()
+        {
+            var tankkaart = GetTankkaart1();
+            await _repo.Add(tankkaart);
+            var tankkaarten = await _repo.GetAll();
+            var tankkaartenActief = await _repo.GetAllActief();
+
+            //Probleem met GetAll?
+            Assert.True(tankkaarten.Count() == tankkaartenActief.Count() + 1 );
+            var addedTankkaart = await _repo.GetById(tankkaart.Kaartnummer);
+            Cleanup(addedTankkaart);
+        }
+
+        [Fact]
+        public async Task Update_()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public async Task Find_()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public async Task Blokeren_()
+        {
+            throw new NotImplementedException();
+        }
+
 
     }
 }
--- Fleetmanagement-app-BLL/Repository/TankkaartRepository.cs
+++ Fleetmanagement-app-BLL/Repository/TankkaartRepository.cs
@@ -15,6 +15,119 @@ namespace Fleetmanagement_app_BLL.Repository
     {
         public TankkaartRepository(FleetmanagerContext context, ILogger logger) : base(context, logger)
         {
+            _context = context;
         }
+
+        public override async Task<bool> Add(Tankkaart tankkaart)
+        {
+            _logger.LogWarning("Tankkaart_Add: Start van Methode");
+            try
+            {
+                List<ToewijzingBrandstofTankkaart> toewijzingenbrandstof = new List<ToewijzingBrandstofTankkaart>();
+                tankkaart.Kaartnummer = tankkaart.Kaartnummer.Trim();
+
+                if (VerplichteVeldenLeeg(tankkaart))
+                {
+                    _logger.LogWarning("Tankkaart_Add: GeldigheidsDatum of Kaartnummer zijn leeg");
+                    return false;
+                }
+
+                if (_dbSet.Where(t => t.Kaartnummer == tankkaart.Kaartnummer).Any())
+                {
+                    _logger.LogWarning("Tankkaart_Add: Er bestaat al een kaart met nummer " + tankkaart.Kaartnummer);
+                    return false;
+                }
+                                
+                if (tankkaart.MogelijkeBrandstoffen != null)
+                {
+                    _logger.LogWarning("Tankkaart_Add: Start Brandstof toewijzingen");
+                    foreach (var brandstof in tankkaart.MogelijkeBrandstoffen)
+                    {
+                        if(!_context.Brandstof.Where(b => b.TypeBrandstof == brandstof.Brandstof.TypeBrandstof).Any())
+                        {
+                            _logger.LogWarning("Tankkaart_Add: Branstof met Type " + brandstof.Brandstof.TypeBrandstof + "bestaat niet");
+                            return false;
+                        }
+                    }
+                }
+
+                tankkaart.LaatstGeupdate = DateTime.Now;
+                await _dbSet.AddAsync(tankkaart);
+                _logger.LogWarning("Tankkaart_Add: Tankkaart toegevoegd aan Database");
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                _logger.LogWarning(e, "Tankkaart_Add: " + e.Message);
+                return false;
+            }
+        }
+        
+        public override async Task<bool> Delete(string kaartnummer)
+        {
+            _logger.LogWarning("Tankkaart_Delete: Start van Methode");
+
+            var tankkaart = await GetById(kaartnummer);
+            tankkaart.IsGearchiveerd = true;
+            _logger.LogWarning("Tankkaart_Delete: IsGearchiveerd is true");
+
+            // verwijder referentie naar koppeling.
+            _logger.LogWarning("Tankkaart_Delete: Tankkaart van koppeling gehaald");
+
+            _logger.LogWarning("Tankkaart_Delete: Einde van Methode");
+            return true;
+        }
+
+        public async Task<IEnumerable<Tankkaart>> GetAllActief()
+        {
+            var alleTankkaarten = await GetAll();
+            var activeTankkaarten = new List<Tankkaart>();
+            foreach (var tankkaart in alleTankkaarten)
+            {
+                if (tankkaart.IsGearchiveerd == false)
+                {
+                    activeTankkaarten.Add(tankkaart);
+                }
+            }
+            return activeTankkaarten;
+        }
+
+        public override async Task<bool> Update(Tankkaart tankkaart)
+        {
+            if (VerplichteVeldenLeeg(tankkaart))
+            {
+                _logger.LogWarning("Tankkaart_Add: GeldigheidsDatum of Kaartnummer zijn leeg");
+                return false;
+            }
+
+            if (_dbSet.Where(t => t.Kaartnummer == tankkaart.Kaartnummer).Any())
+            {
+                _logger.LogWarning("Tankkaart_Add: Er bestaat al een kaart met nummer " + tankkaart.Kaartnummer);
+                return false;
+            }
+            tankkaart.LaatstGeupdate = DateTime.Now;
+            _dbSet.Update(tankkaart);
+            return true;
+        }
+
+        public override async Task<IEnumerable<Tankkaart>> Find(Expression<Func<Tankkaart, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> Blokkeren(string kaartnummer)
+        {
+            var tankkaart = await GetById(kaartnummer);
+            tankkaart.IsGeblokkeerd = true;
+            _dbSet.Update(tankkaart);
+            return true;
+        }
+
+        private bool VerplichteVeldenLeeg(Tankkaart tankkaart)
+        {
+            return (tankkaart.GeldigheidsDatum == default | tankkaart.Kaartnummer == "");
+        }
+
     }
 }
